846b4f5afa5adcf962ebe7168d36bb1ccd6e8dad
Jacob Sendzik <jake.sendzik@gmail.com>
2023-09-01 21:42:41 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function findMatching(array, name) {
+    const newDrivers = []
+    for (const driver of array) {
+        if (driver.toLowerCase() == name.toLowerCase()) {
+            newDrivers.push(driver)
+        }
+    }
+    return newDrivers
+}
+function fuzzyMatch(array, letters) {
+    const newDriver = []
+    for (const driver of array) {
+        if (driver.includes(letters) && letters.length > 1 && letters != "mm"){
+            newDriver.push(driver)
+        }
+    }
+    return newDriver
+}
+function matchName(array, name) {
+    const newDrivers = [];
+    for (const driver of array) {
+        if (driver.name === name) {
+            newDrivers.push(driver)
+        }
+    }
+    return newDrivers
+}
